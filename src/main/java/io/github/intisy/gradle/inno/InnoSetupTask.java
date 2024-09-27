80b10b5c165cca66b3f2992c0d70c51fc20c6ade
Intisy <wilderpizza99@gmail.com>
2024-09-27 18:51:46 +0200

    Update InnoSetupTask.java

--- src/main/java/io/github/intisy/gradle/inno/InnoSetupTask.java
+++ src/main/java/io/github/intisy/gradle/inno/InnoSetupTask.java
@@ -3,42 +3,14 @@ package io.github.intisy.gradle.inno;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
 
 public class InnoSetupTask extends DefaultTask {
-    private String fileName;
-    private String name;
-    private String icon;
-
-    @Input
-    public @NotNull String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Input
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-
-    @Input
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
+    String fileName;
+    String name;
+    String icon;
 
     @TaskAction
     public void createExe() {
