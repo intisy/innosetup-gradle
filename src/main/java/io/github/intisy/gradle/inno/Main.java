095e4a9784d43ff7e2fb24cb6cd4f03ad2cf3b28
finn <wilderpizza99@gmail.com>
2024-11-11 16:38:58 +0100

    Create Main.java

---
+++ src/main/java/io/github/intisy/gradle/inno/Main.java
@@ -0,0 +1,13 @@
+package io.github.intisy.gradle.inno;
+
+import org.gradle.api.Project;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Main class.
+ */
+@SuppressWarnings("unused")
+class Main implements org.gradle.api.Plugin<Project> {
+	@Override
+	public void apply(@NotNull Project target) {}
+}
